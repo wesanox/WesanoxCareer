97933ad4162d5a723a36bf846f3b6f598e683ec1
Andre Wester <wester@mediamus.de>
2025-08-27 08:30:21 +0200

    Add headline and text support in `basic_teaser_career` and `matrix_basic` fields of `WesanoxCareer` module

--- src/fields/basic_teaser_career.php
+++ src/fields/basic_teaser_career.php
@@ -44,28 +44,45 @@ if ($fields->get('dynamic_city') && $fields->get('dynamic_company')) :
 
         $company = ', dynamic_company=' . $company_string;
     }
-    ?>
-    <section class="<?php echo $page->matrix('type'); ?> py-5">
-        <div class="container">
-            <div class="swiper basic_teaser_career_swiper" data-aos="fade-up" data-aos-duration="1500">
-                <div class="swiper-wrapper">
-                    <?php foreach ($pages->find('template=template_career_job' .$limit . $city . $company) as $job) : ?>
-                        <a class="swiper-slide text-black border p-4" href="<?php echo $job->url; ?>" aria-label="Jetzt mehr erfahren über - <?php echo $job->title; ?>">
-                            <small><?php echo $job->dynamic_city[0]; ?></small>
-                            <div class="fs-3 fw-normal py-2">
-                                <?php echo $job->title; ?>
-                            </div>
-                            <small><?php echo $job->dynamic_company[0]; ?></small>
-                        </a>
-                    <?php endforeach; ?>
+
+    $jobs = $pages->find('template=template_career_job' .$limit . $city . $company);
+
+    if( count($jobs) != 0) :
+        ?>
+        <section class="<?php echo $page->matrix('type'); ?> py-5">
+            <div class="container">
+                <div class="pb-5">
+                    <?php echo $modules->get('WesanoxHelperClasses')->getHeadline($page->edit('headline'), $page->headline_tags, $page->headline_class, $page->headline_align); ?>
+                    <div class="text-center pt-2">
+                        <?php echo $page->edit('text'); ?>
+                    </div>
                 </div>
-                <div class="swiper-pagination-career d-block text-center mt-5"></div>
+                <div class="swiper basic_teaser_career_swiper" data-aos="fade-up" data-aos-duration="1500">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($jobs as $job) : ?>
+                            <a class="swiper-slide text-black border p-4" href="<?php echo $job->url; ?>" aria-label="Jetzt mehr erfahren über - <?php echo $job->title; ?>">
+                                <small><?php echo $job->dynamic_city[0]; ?></small>
+                                <div class="fs-3 fw-normal py-2">
+                                    <?php echo $job->title; ?>
+                                </div>
+                                <small><?php echo $job->dynamic_company[0]; ?></small>
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="swiper-pagination-career d-block text-center mt-5"></div>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    <?php endif; ?>
 <?php else : ?>
     <section class="<?php echo $page->matrix('type'); ?> py-5">
         <div class="container">
+            <div class="pb-5">
+                <?php echo $modules->get('WesanoxHelperClasses')->getHeadline($page->edit('headline'), $page->headline_tags, $page->headline_class, $page->headline_align); ?>
+                <div class="text-center pt-2">
+                    <?php echo $page->edit('text'); ?>
+                </div>
+            </div>
             <div class="swiper basic_teaser_career_swiper" data-aos="fade-up" data-aos-duration="1500">
                 <div class="swiper-wrapper">
                     <?php foreach ($pages->find('template=template_career_job' . $limit) as $job) : ?>
